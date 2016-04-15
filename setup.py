09252f85fa6c2e678f10ee9f8ffadabcf51bda33
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2016-04-15 14:51:45 +0200

    Fixed typo

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name=NAME,
                          open(os.path.join("docs", "HISTORY.txt")).read(),
       author='European Environment Agency (EEA)',
       author_email='webadmin@eea.europa.eu',
-      url="https://github.com/eea/eee.rabbitmq.client",
+      url="https://github.com/eea/eea.rabbitmq.client",
       license='GPL',
       install_requires=[
           'pika'
