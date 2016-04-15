4419bb1dc2d05b791a09d8d67a9af368f9a04ee1
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2016-04-15 17:34:20 +0300

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 import os
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
+
 
 NAME = 'eea.rabbitmq.client'
 VERSION = open('version.txt').read().strip()
@@ -15,9 +13,16 @@ setup(name=NAME,
       author='European Environment Agency (EEA)',
       author_email='webadmin@eea.europa.eu',
       url="https://github.com/eea/eea.rabbitmq.client",
-      namespace_packages=['eea'],
+      namespace_packages=['eea', 'eea.rabbitmq'],
+      packages=find_packages(exclude=['ez_setup']),
+      include_package_data=True,
       license='GPL',
+      zip_safe=True,
       install_requires=[
           'pika'
       ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+
       )
