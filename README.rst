ea39c1ff4bd9d86d61822143f8be69987d324bb9
Alexandru Ghica <alec.ghica@eaudeweb.ro>
2016-04-20 13:26:09 +0200

    Updates for eea.rabbitmq.client package
    * Feature: added documentation
      [alecghica refs #69689]
    * Bug fix: fixed package structure
      [alecghica refs #69689]

---
+++ README.rst
@@ -0,0 +1,38 @@
+=========================
+RabbitMQ Python connector
+=========================
+
+Basic RabbitMQ Python connector.
+
+Contents
+========
+
+.. contents::
+
+Introduction
+============
+
+eea.rabbitmq.client is a connector for a RabbitMQ_ server.
+
+.. _RabbitMQ: https://www.rabbitmq.com
+
+API
+===
+
+Usage example::
+
+    from eea.rabbitmq.client import RabbitMQConnector
+    
+    rabbit_config = {
+        'rabbit_host': "10.0.0.1",
+        'rabbit_port': "8080",
+        'rabbit_username': "admin",
+        'rabbit_password': "admin"
+    }
+    queue_name = "QUEUE_NAME"
+    
+    rabbit = RabbitMQConnector(**rabbit_config)
+    rabbit.open_connection()
+    rabbit.declare_queue(queue_name)
+    rabbit.send_message(queue_name, "body text")
+    rabbit.close_connection()
--- setup.py
+++ setup.py
@@ -1,15 +1,29 @@
+""" Installer
+"""
 import os
 from setuptools import setup, find_packages
 
-
 NAME = 'eea.rabbitmq.client'
-VERSION = open('version.txt').read().strip()
+PATH = NAME.split('.') + ['version.txt']
+VERSION = open(os.path.join(*PATH)).read().strip()
 
 setup(name=NAME,
       version=VERSION,
-      description='Python rabbitmq client',
-      long_description = open('README.md').read() + "\n\n" +
+      description='Python RabbitMQ client',
+      long_description = open('README.rst').read() + "\n\n" +
                          open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+          "Framework :: Zope2",
+          "Framework :: Zope3",
+          "Framework :: Plone",
+          "Framework :: Plone :: 4.3",
+          "Programming Language :: Zope",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "License :: OSI Approved :: Mozilla Public License 1.0 (MPL)",
+        ],
+      keywords='eea RabbitMQ plone zope python',
       author='European Environment Agency (EEA)',
       author_email='webadmin@eea.europa.eu',
       url="https://github.com/eea/eea.rabbitmq.client",
