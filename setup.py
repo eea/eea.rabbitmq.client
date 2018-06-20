9d6d83c255232278aee1df8673163ca063e222ba
Alexandru Ghica <alecghica@gmail.com>
2018-06-20 13:03:51 +0000

    updated URLs pointing to eea.europa.eu with https://, refs #95849

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(name=NAME,
       description='Python RabbitMQ client',
       long_description=open('README.rst').read() + "\n\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      # https://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
           "Framework :: Zope2",
           "Framework :: Plone",
